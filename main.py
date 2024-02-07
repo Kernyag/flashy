49ff7a1342994904df24117d7bd62147316fff3d
Kernyag <kernyag@gmail.com>
2024-02-07 17:32:32 +0100

    Added the text files to flash card canvas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from tkinter import *
 from pandas import *
+from tkinter import *
 BACKGROUND_COLOR = "#B1DDC6"
+FONT_LANGUAGE = ("Arial", 40, "italic")
+FONT_WORD = ("Arial", 60, "bold")
 
 #-------------------------------------- UI --------------------------------------#
 # Window
@@ -13,6 +15,8 @@ front_canvas = Canvas(width=800, height=526, highlightthickness=0)
 front_card_img = PhotoImage(file="images\card_front.png")
 front_canvas.create_image(400, 263, image=front_card_img)
 front_canvas.grid(column=0, row=0, columnspan=2)
+language_text = front_canvas.create_text(400, 150, text="German", font=FONT_LANGUAGE)
+word_text = front_canvas.create_text(400, 263, text="Ich", font=FONT_WORD)
 
 # Buttons
 wrong_button_img = PhotoImage(file="images\wrong.png")
@@ -22,6 +26,5 @@ wrong_bt.grid(column=0, row=1)
 right_button_img = PhotoImage(file="images\correct.png")
 right_bt = Button(image=right_button_img, highlightthickness=0)
 right_bt.grid(column=1, row=1)
-
 
 window.mainloop()
